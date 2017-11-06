803a9392c02c2271059d5ab3836dc0e8aca79feb
joppzhang <joppzhang@gmail.com>
2017-11-06 20:06:48 +0800

    day11

--- day11/BST.py
+++ day11/BST.py
@@ -1,22 +1,22 @@
-#*_*coding = utf-8*_*
+# *_*coding = utf-8*_*
 
-#二叉搜索树
+# 二叉搜索树
 
-#二叉树节点，这里我们以data就是key，不考虑有重复元素
+# 二叉树节点，这里我们以data就是key，不考虑有重复元素
 class BstNode(object):
     def __init__(self,data):
         self.left = None
         self.right = None
         self.data = data
 
-#二叉树的操作
+# 二叉树的操作
 class BstTree(object):
     def __init__(self,data):
         self.root = BstNode(data)
 
-    #查找返回值是节点对象，这样方便下面的插入操作。
-    #如果返回存在，就返回本身，和True
-    #否则返回最后一个叶子和False
+    # 查找返回值是节点对象，这样方便下面的插入操作。
+    # 如果返回存在，就返回本身，和True
+    # 否则返回最后一个叶子和False
     def search(self,value):
         r=self.root
         while (r != None):
@@ -38,9 +38,9 @@ class BstTree(object):
         else :
             x.right = i
 
-    #def delete(self,value):
+    # def delete(self,value):
 
-#中序遍历方法，可以得到排序后的结果
+# 中序遍历方法，可以得到排序后的结果
 def tinTree(b,res):
     if b==None :
         return None
@@ -48,7 +48,7 @@ def tinTree(b,res):
     res.append(b.data)
     tinTree(b.right,res)
 
-#主方法Test
+# 主方法Test
 if __name__ == '__main__':
     b=BstTree(8)
     b.insert(2)
